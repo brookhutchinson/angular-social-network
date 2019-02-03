db9705841d98ed514b1e4ff5cbdfd45879bd70a1
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-03 09:12:18 -0600

    Cleanup project files Change "version" property value to "1.0.0" inside package.json Change "version" property value to "1.0.0" inside package-lock.json Change property values inside Angular CLI Config File Change "prefix" property value from "app" to "" inside angular-cli.json Create "css" folder under folder "src/app" Move styles.css from folder "src" to folder "src/app/css" Change "styles" property value file path to "src/app/css/styles.css" inside angular-cli.json Format files Trim files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// define routes
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  // imports
+  imports: [ RouterModule.forRoot(routes) ],
+  // exports
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
