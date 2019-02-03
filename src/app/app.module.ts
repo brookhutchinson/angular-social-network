ae09e2f38333e1d2d542e5b1a119dc16e5c36fa2
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-03 13:48:31 -0600

    Setup Angular Material Use Angular CLI 'ng add' command to setup, install, and configure Angular Material Install npm package named @angular/material as a runtime dependency inside package.json Install npm package named @angular/cdk as a runtime dependency inside package.json Download the npm package named @angular/material into the "node_modules" folder Download the npm package named @angular/cdk into the "node_modules" folder Install an Angular Material Theme by adding the selected theme to the Angular CLI Config File angular.json using the "styles" property array Add a <link> html element to index.html to use the selected  Angulr Material Theme font icons Add a <link> html element to index.html to use the selected Angular Material Theme text font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 // modules
-import { NgModule }         from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule }                from '@angular/core';
+import { AppRoutingModule }        from './app-routing.module';
+import { BrowserModule }           from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule }             from '@angular/forms';
+import { HttpClientModule }        from '@angular/common/http';
 
 // components
-import { AppComponent }     from './app.component';
+import { AppComponent }            from './app.component';
 
 @NgModule({
   // modules
-  imports: [ AppRoutingModule, BrowserModule, FormsModule, HttpClientModule ],
+  imports: [ AppRoutingModule, BrowserModule, BrowserAnimationsModule, FormsModule, HttpClientModule ],
   // components
   declarations: [ AppComponent ],
   // services
