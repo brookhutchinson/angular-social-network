1e81cc1af1627ac4964852c36b48d3274c778c9f
Brook Hutchinson <Brookhutchinson@outlook.com>
2019-02-04 09:09:57 -0600

    Define PostCreateComponent Logic Allow a user to enter a new post inside a <textarea> html element Define a property named "enteredPost" of type string Set initial value of "enteredPost" property to empty string inside ngOnInit() method Use the "enteredPost" property, the ngModel directive, and two-way data binding to bind the value of the <textarea> html element to the "enteredPost" property The ngModel directive will listen to the keypress() event of the <textarea> html element The ngModel directive will store the user entered post inside a property named "enteredPost" on each keypress On the click event of the Add Post Button set the "newPost" property value to the "enteredPost" property value Define a property anmed "newPost" of type string Set initial value of "newPost" property to empty string inside ngOnInit() method Set the user entered post that is stored inside the "enteredPost" property to the "newPost" property Use string interpolation and a data binding expression to render "newPost" property value inside the PostCreateComponent HTML Template

--- src/app/components/post-create/post-create.component.ts
+++ src/app/components/post-create/post-create.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [ './post-create.component.css' ]
 })
 export class PostCreateComponent implements OnInit {
+  // properties
+  enteredPost: string;
+  newPost: string;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // set properties
+    this.enteredPost = '';
+    this.newPost = '';
+  }
+
+  onAddPost() {
+    // update property
+    this.newPost = this.enteredPost;
+  }
 }
